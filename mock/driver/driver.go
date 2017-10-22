f1d83a963e017e857bf0af4ae95015ddf7fc617d
Luis Pabón <luis@portworx.com>
2017-10-22 10:56:46 -0400

    rpc working

--- mock/driver/driver.go
+++ mock/driver/driver.go
@@ -3,10 +3,59 @@ package driver
 //go:generate mockgen -source=$GOPATH/src/github.com/container-storage-interface/spec/lib/go/csi/csi.pb.go -imports .=github.com/container-storage-interface/spec/lib/go/csi -package=driver -destination=driver.mock.go
 import (
 	"fmt"
-	//	_ "github.com/container-storage-interface/spec/lib/go/csi"
+	"net"
+	"sync"
+
+	"github.com/container-storage-interface/spec/lib/go/csi"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 // Check is To be removed
 func Check() {
 	fmt.Printf("Check")
+}
+
+type MockCSIDriverServer struct {
+	Listener net.Listener
+	server   *grpc.Server
+	wg       sync.WaitGroup
+}
+
+func NewMockCSIDriver() *MockCSIDriverServer {
+	return &MockCSIDriverServer{}
+}
+
+func (m *MockCSIDriverServer) goServe() {
+	m.wg.Add(1)
+	go func() {
+		defer m.wg.Done()
+		m.server.Serve(m.Listener)
+	}()
+}
+
+func (m *MockCSIDriverServer) Address() string {
+	return m.Listener.Addr().String()
+}
+func (m *MockCSIDriverServer) Start(s csi.IdentityServer) error {
+
+	// Listen on a port assigned by the net package
+	l, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		return err
+	}
+	m.Listener = l
+
+	m.server = grpc.NewServer()
+	csi.RegisterIdentityServer(m.server, s)
+	reflection.Register(m.server)
+
+	m.goServe()
+	return nil
+}
+
+func (m *MockCSIDriverServer) Stop() error {
+	m.server.Stop()
+	m.wg.Wait()
+	return nil
 }
--- mock/driver_test.go
+++ mock/driver_test.go
@@ -1,4 +1,4 @@
-package driver
+package mock
 
 import (
 	"testing"
@@ -6,6 +6,8 @@ import (
 	"github.com/container-storage-interface/spec/lib/go/csi"
 	mock_driver "github.com/csi-volumes/kubernetes-csi/mock/driver"
 	gomock "github.com/golang/mock/gomock"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
 )
 
 func TestPluginInfoResponse(t *testing.T) {
@@ -13,7 +15,7 @@ func TestPluginInfoResponse(t *testing.T) {
 	// Setup mock
 	m := gomock.NewController(t)
 	defer m.Finish()
-	driver := mock_driver.NewMockIdentityClient(m)
+	driver := mock_driver.NewMockIdentityServer(m)
 
 	// Setup input
 	in := &csi.GetPluginInfoRequest{
@@ -44,7 +46,67 @@ func TestPluginInfoResponse(t *testing.T) {
 	r, err := driver.GetPluginInfo(nil, in)
 	name := r.GetResult().GetName()
 	if err != nil {
-		t.Errorf("Error")
+		t.Errorf("Error: %s", err.Error())
+	}
+	if name != "mock" {
+		t.Errorf("Unknown name: %s\n", name)
+	}
+}
+
+func TestGRPCGetPluginInfoReponse(t *testing.T) {
+
+	// Setup mock
+	m := gomock.NewController(t)
+	defer m.Finish()
+	driver := mock_driver.NewMockIdentityServer(m)
+
+	// Setup input
+	in := &csi.GetPluginInfoRequest{
+		Version: &csi.Version{
+			Major: 0,
+			Minor: 1,
+			Patch: 0,
+		},
+	}
+
+	// Setup mock outout
+	out := &csi.GetPluginInfoResponse{
+		Reply: &csi.GetPluginInfoResponse_Result_{
+			Result: &csi.GetPluginInfoResponse_Result{
+				Name:          "mock",
+				VendorVersion: "0.1.1",
+				Manifest: map[string]string{
+					"hello": "world",
+				},
+			},
+		},
+	}
+
+	// Setup expectation
+	// !IMPORTANT!: Must set context expected value to gomock.Any() to match any value
+	driver.EXPECT().GetPluginInfo(gomock.Any(), in).Return(out, nil).Times(1)
+
+	// Create a new RPC
+	server := mock_driver.NewMockCSIDriver()
+	err := server.Start(driver)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	defer server.Stop()
+
+	// Actual call
+	conn, err := grpc.Dial(server.Address(), grpc.WithInsecure())
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	defer conn.Close()
+
+	// Make call
+	c := csi.NewIdentityClient(conn)
+	r, err := c.GetPluginInfo(context.Background(), in)
+	name := r.GetResult().GetName()
+	if err != nil {
+		t.Errorf("Error: %s", err.Error())
 	}
 	if name != "mock" {
 		t.Errorf("Unknown name: %s\n", name)
